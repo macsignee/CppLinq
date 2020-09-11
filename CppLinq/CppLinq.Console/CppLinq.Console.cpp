c289e3f247c0383c810328a19a65c85ae9d466e0
Shigene Makoto <macsignee@gmail.com>
2020-09-11 11:26:35 +0900

    write sample code

--- CppLinq/CppLinq.Console/CppLinq.Console.cpp
+++ CppLinq/CppLinq.Console/CppLinq.Console.cpp
@@ -7,6 +7,14 @@ int main()
 {
     std::cout << "Hello World!\n";
 
-    std::string str = "aaaaaaa";
-    auto a = From(str);
+    auto n10 = From<std::vector<int>>({ 2, 3, 1, 4, 5, 6, 9, 8, 7, 14, 13, 22 })
+        //auto n10 = From<std::list<int>>({ 2, 3, 1, 4, 5, 6, 9, 8, 7, 14, 13, 22 }) // also Ok
+        .Where([](const auto& elm) {return elm % 2 == 1; })
+        //.SortBy([](const auto& lhs, const auto& rhs) {return lhs < rhs; })
+        .OrderBy([](const auto& elm) {return elm; }, [](const auto& lhs, const auto& rhs) {return lhs < rhs; })
+        .Select(
+            [](const auto& elm) {
+                return std::make_tuple(elm, (double)elm, std::to_string(elm) + "-str");
+            })
+        .ToVector();
 }
